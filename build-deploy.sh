b48694b89135d48f9417b1156f588b76e573fcc8
John Kojo Ampia-Addison <me@jojoaddison.net>
2021-08-25 11:56:45 +0200

    Version: 0.3.3

---
+++ build-deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+args=$#
+
+if [ $args -le 0 ];then
+    echo "version number required eg. 0.0.5"
+    exit 1
+fi
+   
+
+# Set the version for the build and deploy
+export version=$1
+echo "Building and Deploying to Jojo Addison Web Application version $version"
+
+echo "building..."
+./mvnw -Pprod clean verify jib:dockerBuild -DskipTests
+echo "done."
+
+echo "tagging..."
+docker tag jaweb docker-registry.jojoaddison.net/jaweb:$version
+docker image ls | grep 'jaweb'
+echo "done."
+
+echo "pushing..."
+docker push docker-registry.jojoaddison.net/jaweb:$version
+echo "done."
+echo "build and deploy completed."
